f539948cb4ec3add5a87cd830c081d46ae36698e
Tw Shim <marocchino@gmail.com>
2014-01-12 21:43:34 +0900

    Add all, table method

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,21 @@
-Card = Struct.new(:bg, :color, :shape, :location)
+Card = Struct.new(:bg, :color, :shape, :location) do
+  BGS = [:black, :gray, :white]
+  COLORS = [:red, :yello, :blue]
+  SHAPES = [:circle, :rect, :triangle]
+  def self.all
+    BGS.map do |bg|
+      COLORS.map do |color|
+        SHAPES.map do |shape|
+          new(bg, color, shape, nil)
+        end
+      end
+    end.flatten
+  end
+
+  def self.table
+    i = 0
+    all.sort_by { rand }[0, 9].each do |card|
+      card.location = i += 1
+    end
+  end
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,4 +5,8 @@ describe Card do
   it { expect(Card.new(:black, :yello, :circle, 1).color).to eq(:yello) }
   it { expect(Card.new(:black, :yello, :circle, 1).shape).to eq(:circle) }
   it { expect(Card.new(:black, :yello, :circle, 1).location).to be(1) }
+  it { expect(Card.all.size).to be(27) }
+  it { expect(Card.table.size).to be(9) }
+  it { expect(Card.table.first.location).to be(1) }
+  it { expect(Card.table.last.location).to be(9) }
 end
